603e43d8c1581ebf308b2e71acf8779a6621f77d
iveni1 <168824571+iveni1@users.noreply.github.com>
2024-05-03 22:49:28 +0300

    Update Login.java

--- Login.java
+++ Login.java
@@ -1,7 +1,7 @@
-public boolean login(String username, String password) {
-    User user = users.get(username);
-    if (user != null && user.getPassword().equals(password)) {
-        return true;
+public class login extends UserService {
+    
+    public boolean login(String username, String password) {
+        User user = users.get(username);
+        return user != null && user.getPassword().equals(password);
     }
-    return false;
 }
