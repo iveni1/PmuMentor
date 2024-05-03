97eff95303f940bb42fe6211d36b690bb1a30fbd
iveni1 <168824571+iveni1@users.noreply.github.com>
2024-05-03 23:11:37 +0300

    Delete Login.java

--- Login.java
+++
@@ -1,7 +0,0 @@
-public class login extends UserService {
-    
-    public boolean login(String username, String password) {
-        User user = users.get(username);
-        return user != null && user.getPassword().equals(password);
-    }
-}
